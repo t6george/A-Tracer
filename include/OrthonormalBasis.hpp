604479b88ff6b0838812e68e49f78715e9c94383
ThomasGeorge <t6george@edu.uwaterloo.ca>
2020-07-28 00:23:27 -0400

    added some moss

---
+++ include/OrthonormalBasis.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <array>
+#include <Vec3.hpp>
+
+class OrthonormalBasis
+{
+    std::array<Vec3, 3> axes;
+
+public:
+    OrthonormalBasis(const Vec3& sample);
+    ~OrthonormalBasis() noexcept = default;
+
+    Vec3 operator[](int i) const;
+
+    Vec3 getU() const;
+    Vec3 getV() const;
+    Vec3 getW() const;
+
+    Vec3 getVec(double a, double b, double c) const;
+    Vec3 getVec(Vec3 v) const;
+};
--- src/LambertianDiffuse.cpp
+++ src/LambertianDiffuse.cpp
@@ -1,19 +1,24 @@
 #include <LambertianDiffuse.hpp>
 #include <Texture.hpp>
+#include <OrthonormalBasis.hpp>
 
 LambertianDiffuse::LambertianDiffuse(const std::shared_ptr<Texture> albedo)
     : Material::Material{albedo} {}
 
 bool LambertianDiffuse::scatterRay(const Ray &ray, Hittable::HitRecord &record) const
 {
+    OrthonormalBasis onb(record.normal);
+
     record.scatteredRay.setOrigin(record.point);
     //record.scatteredRay.setDirection((record.normal + Vec3::randomUnitSphereVec()).getUnitVector());
-    record.scatteredRay.setDirection(Vec3::randomUnitHemisphereVec(record.normal));
+    //record.scatteredRay.setDirection(Vec3::randomUnitHemisphereVec(record.normal));
+    record.scatteredRay.setDirection(Vec3::randomCosineVec().getUnitVector());
     record.scatteredRay.setTime(ray.getTime());
     record.albedo = albedo->getValue(record.u, record.v, record.point);
     record.emitted = emitCol(record.u, record.v, record.point);
     //record.samplePdf = record.normal.o(record.scatteredRay.getDirection()) / utils::pi;
-    record.samplePdf = .5 / utils::pi;
+    //record.samplePdf = .5 / utils::pi;
+    record.samplePdf = onb.getW().o(record.scatteredRay.getDirection()) / utils::pi;
     scatterPdf(ray, record);
     return true;
 }
